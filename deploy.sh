30ee0c9969775b6e9e7b5d37519f0f13384968cc
venkatnaresh <venkatnaresh359@gmail.com>
2018-09-05 12:06:23 +0530

    modified file deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ $ENVIRONMENT = "QA" ];then
         sshpass -p "naresh" scp target/mavenwar.war naresh@172.17.0.2:/home/softcopies/apache-tomcat-8.5.32/webapps
         sshpass -p "naresh" ssh naresh@172.17.0.2 "JAVA_HOME=/home/softcopies/jdk1.8.0_181/home/softcopies/apache-tomcat-8.5.32/bin/startup.sh"
